95dcd908ddd20c3f8d12a67a89efe0aacd50e1c2
Aziz Tutucu <aziztutucu99@gmail.com>
2022-08-01 17:52:03 +0300

    Updated

--- src/KdvTutar.java
+++ src/KdvTutar.java
@@ -4,11 +4,16 @@ public class KdvTutar {
     public static void main(String[] args) {
         double kdvliTutar,kdvsizTutar,tutar,kdvTutari;
         double kdv = 0.18;
+        boolean kdvDurumu;
+
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Tutar giriniz:" );
-        tutar = input.nextInt();
+        tutar = input.nextDouble();
+
+        kdvDurumu = (0<tutar)&&(tutar<1000);
+        kdv= kdvDurumu ? 0.18 : 0.08;
 
         kdvliTutar=  (tutar + (tutar*kdv));
         kdvsizTutar= tutar;
