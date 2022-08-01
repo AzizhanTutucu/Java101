7edc55f4c690574722c3f182d03b36337a00e2cb
Aziz Tutucu <aziztutucu99@gmail.com>
2022-08-01 17:02:44 +0300

    Updated

---
+++ src/Giris/NotOrtalaması.java
@@ -0,0 +1,38 @@
+package Giris;
+
+import java.util.Scanner;
+
+
+public class NotOrtalaması {
+    public static void main(String[] args) {
+        int mat,fizik,tarih,kimya,türkce;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Mat notunu giriniz:");
+        mat = input.nextInt();
+
+        System.out.print("Fizik notunu giriniz:");
+        fizik = input.nextInt();
+
+
+        System.out.print("Tarih notunu giriniz:");
+        tarih = input.nextInt();
+
+
+        System.out.print("Kimya notunu giriniz:");
+        kimya = input.nextInt();
+
+        System.out.print("Turkce notunu giriniz:");
+        türkce = input.nextInt();
+
+        int toplam = (mat+fizik+tarih+kimya+türkce);
+        double sonuc = toplam/6;
+        System.out.println("Ortalamaniz:"+ sonuc);
+
+        String str= sonuc>=60 ? "Gectiniz" : "Kaldiniz";
+        System.out.println(str);
+
+
+
+    }
+}
