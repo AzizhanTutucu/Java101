18c87fb6b8dc005e5bc5bd255efcbc44d3a3f464
Aziz Tutucu <aziztutucu99@gmail.com>
2022-08-09 15:49:28 +0300

    Updated

--- src/MükemmelSayi.java
+++ src/MükemmelSayi.java
@@ -8,7 +8,7 @@ public class MükemmelSayi {
         int toplam =0;
         for (int i = 1 ; i <number; i++){
             if (number%i==0){
-
+                System.out.println("Bolenler : " + i);
                 toplam+=i;
             }
 
